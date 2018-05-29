3cfc85deee8a50f6c8720f47feef025dd1708675
Mustika Sari <37572117+mustikasari18@users.noreply.github.com>
2018-05-29 09:32:03 +0800

    Add files via upload

---
+++ tugasperkalian.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectsaya;
+
+/**
+ *
+ * @author asus
+ */
+public class tugasperkalian {
+    public static void main(String[] args) {
+        int a=8;
+        for(int i=1; i<=10; i++){
+            System.out.println(i+" x "+a+" = "+i*a);
+        }
+    }
+}
